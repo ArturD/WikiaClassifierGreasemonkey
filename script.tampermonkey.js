b3b31a06b0da8bb8d3a266075ad5463e387eb794
Artur Dwornik <artur.dwornik@wikia-inc.com>
2013-08-13 15:18:21 +0200

    fix indenting and use "native" GM_xmlhttprequest

--- script.tampermonkey.js
+++ script.tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Wikia classifier
 // @namespace  http://wikia.com/
-// @version    0.4.2
+// @version    0.4.5
 // @description  Tools for classifier
 // @match        http://*.wikia.com/*
 // @match        http://www.wowwiki.com/*
@@ -29,6 +29,7 @@ var jq = jQuery.noConflict();
 //        ....
 //     }
 //   });
+
 function GM_XHR() {
     this.type = null;
     this.url = null;
@@ -39,23 +40,25 @@ function GM_XHR() {
     this.headers = {};
     this.readyState = null;
 
-    this.abort = function() {
+    this.abort = function () {
         this.readyState = 0;
     };
 
-    this.getAllResponseHeaders = function(name) {
-      if (this.readyState!=4) return "";
-      return this.responseHeaders;
+    this.getAllResponseHeaders = function (name) {
+        if (this.readyState != 4) return "";
+        return this.responseHeaders;
     };
 
-    this.getResponseHeader = function(name) {
-      var regexp = new RegExp('^'+name+': (.*)$','im');
-      var match = regexp.exec(this.responseHeaders);
-      if (match) { return match[1]; }
-      return '';
+    this.getResponseHeader = function (name) {
+        var regexp = new RegExp('^' + name + ': (.*)$', 'im');
+        var match = regexp.exec(this.responseHeaders);
+        if (match) {
+            return match[1];
+        }
+        return '';
     };
 
-    this.open = function(type, url, async, username, password) {
+    this.open = function (type, url, async, username, password) {
         this.type = type ? type : null;
         this.url = url ? url : null;
         this.async = async ? async : null;
@@ -63,12 +66,12 @@ function GM_XHR() {
         this.password = password ? password : null;
         this.readyState = 1;
     };
-    
-    this.setRequestHeader = function(name, value) {
+
+    this.setRequestHeader = function (name, value) {
         this.headers[name] = value;
     };
 
-    this.send = function(data) {
+    this.send = function (data) {
         this.data = data;
         var that = this;
         // http://wiki.greasespot.net/GM_xmlhttpRequest
@@ -77,16 +80,22 @@ function GM_XHR() {
             url: this.url,
             headers: this.headers,
             data: this.data,
-            onload: function(rsp) {
+            onreadystatechange: function (rsp) {
                 // Populate wrapper object with returned data
                 // including the Greasemonkey specific "responseHeaders"
                 for (var k in rsp) {
+                    if (k == "onreadystatechange" || k == "onload") {
+                        continue;
+                    }
                     that[k] = rsp[k];
                 }
                 // now we call onreadystatechange
                 that.onreadystatechange();
             },
-            onerror: function(rsp) {
+            onload: function () {
+                that.onload();
+            },
+            onerror: function (rsp) {
                 for (var k in rsp) {
                     that[k] = rsp[k];
                 }
@@ -96,121 +105,138 @@ function GM_XHR() {
 };
 
 (function (window, unsafeWindow, $) {
-    
+
     // avoid iframes
     if (unsafeWindow.top != unsafeWindow.self) return;
 
-    
+
 })(window, unsafeWindow, jq);
 
 (function (jQuery) {
     var styles = ".classificator ul { padding-left: 10px }\n" +
         ".classificator .label { padding: 0 3px; font-weight: bold; }";
-    var hosts = [ "http://db-sds-s2:8081/graph-0.3.1", "http://db-sds-s1:8081/graph-0.3.1", "http://localhost:8080", "http://localhost:9998" ];
+    var hosts = ["http://db-sds-s2:8081/graph-0.3.1", "http://db-sds-s1:8081/graph-0.3.1", "http://localhost:8080", "http://localhost:9998"];
     var $ = jQuery;
-    var types = [ "other", "tv_episode", "tv_series", "tv_season", "ability", "unit", "person", "organization", "level", "achievement", "location", "item", "weapon", "book", "movie", "game", "character", "music_band", "music_recording", "music_album", "vehicle" ];
+    var types = ["other", "tv_episode", "tv_series", "tv_season", "ability", "unit", "person", "organization", "level", "achievement", "location", "item", "weapon", "book", "movie", "game", "character", "music_band", "music_recording", "music_album", "vehicle"];
     var storageUrl = "http://megimikos.pl:5984/facts/";
     if (unsafeWindow.top != unsafeWindow.self) return;
-    
-    
-    function getDomainFromUrl( url ) {
-        if( url.indexOf('http://') == 0 ) {
+
+
+    function getDomainFromUrl(url) {
+        if (url.indexOf('http://') == 0) {
             return url.split('/')[2];
         } else {
-       		return null;
+            return null;
         }
     }
-    
-    function getPageInfo() {     
+
+    function getPageInfo() {
         var info = {};
-        info.url       = $('link[rel=canonical]').attr('href') || unsafeWindow.location.href;
+        info.url = $('link[rel=canonical]').attr('href') || unsafeWindow.location.href;
         info.articleId = unsafeWindow.wgArticleId;
-        info.cityId    = parseInt( unsafeWindow.wgCityId, 10 );
-        info.title     = unsafeWindow.wgTitle;
+        info.cityId = parseInt(unsafeWindow.wgCityId, 10);
+        info.title = unsafeWindow.wgTitle;
         info.namespace = unsafeWindow.wgNamespaceNumber;
-        info.lang      = unsafeWindow.wgContentLanguage;
-        info.domain    = getDomainFromUrl( info.url );
+        info.lang = unsafeWindow.wgContentLanguage;
+        info.domain = getDomainFromUrl(info.url);
         return info;
-	}
-    
-    function render( obj ) {
+    }
+
+    function render(obj) {
         var div = $('<div>');
         div.html('<b style="font-weight:bold">' + obj.class + '</b>');
         var ul = $('<ul>');
-        for( var i in obj.classes ) {
-        	var cls = obj.classes[i];
+        for (var i in obj.classes) {
+            var cls = obj.classes[i];
             var li = $('<li>');
-            li.css({color: "#888"});
-            li.html( "<span style=\"display: inline-block; min-width: 120px;\">" + i + ":</span> " + (Math.round(cls*10000)/10000) );
-            ul.append( li );
+            li.css({
+                color: "#888"
+            });
+            li.html("<span style=\"display: inline-block; min-width: 120px;\">" + i + ":</span> " + (Math.round(cls * 10000) / 10000));
+            ul.append(li);
         }
         div.append(ul);
         return div;
     }
-    
-    function getCurrentClassificationFromCouch( callback, errorCallback ) {
+
+    function getCurrentClassificationFromCouch(callback, errorCallback) {
         var info = getPageInfo();
-        $.ajax({
-            type: "GET",
-            xhr: function() { new GM_XHR(); },
-            url: storageUrl + encodeURIComponent( info.url ),
-            success: function(x) {
-            	callback(x);
-            }, error: function(x) {
-              errorCallback(x);
-            },
-            dataType: "json"
-        });
+
+                    var req = GM_xmlhttpRequest({
+                        method: "GET",
+                        url: storageUrl + encodeURIComponent(info.url),
+                        onload: function (response) {
+                            GM_log(response);
+                            if (response.status == 200) {
+                                GM_log("get success");
+                                GM_log( response );
+                                callback( JSON.parse( response.responseText ) );
+                            } else {
+                                GM_log( "get failed: " + response.status );
+                                GM_log( response );
+                                errorCallback( );
+                            }
+                        },
+                        onerror: function () {
+                            GM_log( "get error" );
+                            errorCallback();
+                        }
+                    });
     }
 
-    function getCurrentRevisionFromCouch( callback ) {
-      getCurrentClassificationFromCouch( function( data ) {
-        callback( data._rev );
-      }, function() {
-        callback();
-      });
+    function getCurrentRevisionFromCouch(callback) {
+        getCurrentClassificationFromCouch(function (data) {
+            callback(data._rev);
+        }, function () {
+            callback();
+        });
     }
 
     function updateCurrentClass() {
         $('.type-placeholder').text('loading ...');
-        getCurrentClassificationFromCouch( function(data) {
-        	$('.type-placeholder').text(data.type);
-        }, function() {
-                $('.type-placeholder').text( " ?? " );
-        } );
+        getCurrentClassificationFromCouch(function (data) {
+            $('.type-placeholder').text(data.type);
+        }, function () {
+            $('.type-placeholder').text(" ?? ");
+        });
     }
-    
+
     function renderUpdater() {
         var div = $('<div>');
-        for( var i in types ) {
+        for (var i in types) {
             var info = getPageInfo();
-        	var type = types[i];
+            var type = types[i];
             var button = $('<button data-type="' + type + '" value="' + type + '">' + type + '</button>');
-            button.click(function() {
+            button.click(function () {
                 var type = $(this).data('type');
                 var info = getPageInfo();
-                GM_log( "click: " + type );
-            	getCurrentRevisionFromCouch( function( currentVersion ) {
+                GM_log("click: " + type);
+                getCurrentRevisionFromCouch(function (currentVersion) {
                     info.type = type;
-                    if( currentVersion ) {
-                      info._rev = currentVersion;
-                      GM_log( "rev: " + info._rev);
+                    if (currentVersion) {
+                        info._rev = currentVersion;
+                        GM_log("rev: " + info._rev);
                     }
-                    $.ajax({
-                        type: "PUT",
-                        xhr: function() { new GM_XHR(); },
-                        url: storageUrl + encodeURIComponent( info.url ),
+                    var req = GM_xmlhttpRequest({
+                        method: "PUT",
+                        url: storageUrl + encodeURIComponent(info.url),
                         data: JSON.stringify(info),
-                        success: function(x) {
-                            GM_log("put success");
-                            GM_log(x);
-                            updateCurrentClass();
-                        }, error: function(x) {
-                            GM_log("put success");
-                            GM_log(x);
+                        headers: {"Content-Type": "application/json"},
+                        onload: function (response) {
+                            GM_log(response);
+                            if (response.status == 201) {
+                                GM_log("put success");
+                                GM_log( response.responseText );
+                                updateCurrentClass();
+                            } else {
+                                GM_log( "put failed: " + response.status );
+                                //callback(null);
+                            }
                         },
-                        dataType: "json"
-                    });       
+                        onerror: function () {
+                            GM_log("put error");
+                        }
+                    });
                 });
             });
             div.append(button);
@@ -218,40 +244,43 @@ function GM_XHR() {
         return div;
     }
     updateCurrentClass();
-    
+
     function getClassificationsFromHost(wiki, page, callback, host) {
         GM_log(host + "/classifications/" + wiki + "/" + page);
-    	var req = GM_xmlhttpRequest({
-          method: "GET",
-          url: host + "/classifications/" + wiki + "/" + page,
-          onload: function(response) {
-            GM_log(response);
-            if( response.status != 200 ) {
-            	callback(null);
-        	} else {
-            	var obj = JSON.parse(response.responseText);
-            	callback(obj);
-    		}
-          },
- 		  onerror: function() { callback(null); } 
+        var req = GM_xmlhttpRequest({
+            method: "GET",
+            url: host + "/classifications/" + wiki + "/" + page,
+            onload: function (response) {
+                GM_log(response);
+                if (response.status != 200) {
+                    callback(null);
+                } else {
+                    var obj = JSON.parse(response.responseText);
+                    callback(obj);
+                }
+            },
+            onerror: function () {
+                callback(null);
+            }
         });
     }
+
     function getClassifications(wiki, page, callback, hostList) {
-        if( !hostList ) hostList = hosts;
-        if ( hostList.length == 0 ) {
-        	//callback( null );
+        if (!hostList) hostList = hosts;
+        if (hostList.length == 0) {
+            //callback( null );
             return;
         }
-        getClassificationsFromHost(wiki, page, function( response ) {
-            if( response == null ) {
-            	getClassifications( wiki, page, callback, hostList.slice(1) );
+        getClassificationsFromHost(wiki, page, function (response) {
+            if (response == null) {
+                getClassifications(wiki, page, callback, hostList.slice(1));
             } else {
-				callback( response );                
+                callback(response);
             }
         }, hostList[0]);
     }
-     
-    $(function() {
+
+    $(function () {
         var box = $('<div class="description classificator">');
         box.css({
             position: 'absolute',
@@ -266,15 +295,17 @@ function GM_XHR() {
             padding: '10px 20px',
             'max-width': '300px'
         });
-        box.hover(function() {
-            box.css({opacity: 1});
-              }, function() {
+        box.hover(function () {
+            box.css({
+                opacity: 1
+            });
+        }, function () {
             //box.css({opacity: 0.6});
-        });    
+        });
         box.append('<h2>' + getPageInfo().domain + '</h2>');
         box.append('<h4>' + getPageInfo().title + '</h4>');
-        getClassifications(getPageInfo().domain, getPageInfo().title, function(obj) {
-        	var d = render(obj);
+        getClassifications(getPageInfo().domain, getPageInfo().title, function (obj) {
+            var d = render(obj);
             box.append(d);
         });
         box.append('<div class="type-placeholder">loading ... </div>');
